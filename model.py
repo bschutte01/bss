28599a16bec3502c28e77935473f2b9d01882f12
bschutte01 <bschutte@unomaha.edu>
2023-11-12 11:43:16 -0600

    changed state var to product

--- model.py
+++ model.py
@@ -10,7 +10,6 @@ file_name = 'nodal_input.xls'
 path_to_file = Path(file_loc)/file_name
 
 df = pd.read_excel(path_to_file)
-#what to do about blank values?
 
 df['day'] = df['date_time'].dt.date
 df['hour'] = df['date_time'].dt.hour
@@ -39,18 +38,18 @@ try:
     rt_eff = .85    #round trip efficiency, todo: break this down into components based on Nathan's input
     duration = 360    #duration of battery in minutes, inputs will be in 1,2,4,6,8 hours
 
-    #states the battery can be in
+    #products the battery can be in
     ### in order they are idle, charge, discharge, spinning reserve,
     ### supplemental reserve, regulation up, regulation down
-    states = ['i','c','d','spinr','supr','regu','regd']
-    DA_states = ['DAi','DAc','DAd','DAspinr','DAsupr','DAregu','DAregd']
+    products = ['i','c','d','spinr','supr','regu','regd']
+    DA_products = ['DAi','DAc','DAd','DAspinr','DAsupr','DAregu','DAregd']
 
     # Charging params #
     init_SOC = .25      #state of charge prior to running model
-    cd = {states[i]:[0,1*rt_eff,-1,-1,-1,-1,1*rt_eff][i] for i in range(len(states))}    #control if state is charge or discharge
-    TP_eff = {states[i]:[0,1,1,0.1,0,0.2,0.2][i] for i in range(len(states))}    #TP efficiency for each state
-    J = {s:TP_eff[s]*cd[s]*(t_delta/duration) for s in states}
-    DAJ = {f'DA{k}': v for k,v in J.items()} #amount of energy added/removed for each state
+    cd = {products[i]:[0,1*rt_eff,-1,-1,-1,-1,1*rt_eff][i] for i in range(len(products))}    #control if product is charge or discharge
+    TP_eff = {products[i]:[0,1,1,0.1,0,0.2,0.2][i] for i in range(len(products))}    #TP efficiency for each product
+    J = {s:TP_eff[s]*cd[s]*(t_delta/duration) for s in products}
+    DAJ = {f'DA{k}': v for k,v in J.items()} #amount of energy added/removed for each product
 
     #is 20% TP accurate for ancillary market rates? how sensitive is market participation to this TP?
 
@@ -59,8 +58,8 @@ try:
     #just generate some data randomly, need to get this from an input file
     P = df
     DAP = pd.DataFrame(np.random.normal(loc = 1.0, scale =15,
-                                        size = (df.shape[0],len(DA_states))),
-                                        columns=DA_states)
+                                        size = (df.shape[0],len(DA_products))),
+                                        columns=DA_products)
     P = pd.concat([P,DAP], axis = 1)
 
 
@@ -73,11 +72,11 @@ try:
     ### Decision Variables ###
     ##########################
 
-    state = m.addVars(t_horizon,states ,vtype=GRB.BINARY,name = 'state')
-    # state[1,'c'] = 0/1
-    # states = ['i','c','d','spinr','supr','regu','regd']
+    product = m.addVars(t_horizon,products ,vtype=GRB.BINARY,name = 'product')
+    # product[1,'c'] = 0/1
+    # products = ['i','c','d','spinr','supr','regu','regd']
 
-    DA_state = m.addVars(t_horizon,DA_states ,vtype=GRB.BINARY,name = 'DA_state')
+    DA_product = m.addVars(t_horizon,DA_products ,vtype=GRB.BINARY,name = 'DA_product')
 
     SoC = m.addVars(t_horizon, vtype = GRB.CONTINUOUS, name = 'SoC',
                     lb = soc_min,
@@ -86,31 +85,31 @@ try:
     ##########################
     ### Objective Function ###
     ##########################
-    m.setObjective(gp.quicksum(P[j][i]*state[i,j] for i in range(t_horizon) for j in states)
-                   + gp.quicksum(P[j][i]*DA_state[i,j] for i in range(t_horizon) for j in DA_states), 
+    m.setObjective(gp.quicksum(P[j][i]*product[i,j] for i in range(t_horizon) for j in products)
+                   + gp.quicksum(P[j][i]*DA_product[i,j] for i in range(t_horizon) for j in DA_products), 
                    GRB.MAXIMIZE)
 
     ##########################
     ###### Constraints #######
     ##########################
 
-    # battery can only be in 1 state in a given timeframe
+    # battery can only be in 1 product in a given timeframe
     m.addConstrs(
-        (state.sum(t,'*') + DA_state.sum(t,'*') == 1 for t in range(t_horizon)), 
+        (product.sum(t,'*') + DA_product.sum(t,'*') == 1 for t in range(t_horizon)), 
         name = 'single_assignment'
     )
 
     #intial state of charge
     m.addConstr(
-        SoC[0] == init_SOC + (gp.quicksum(J[k]*state[0,k] for k in states)
-                              +gp.quicksum(DAJ[k]*DA_state[0,k] for k in DA_states)),
+        SoC[0] == init_SOC + (gp.quicksum(J[k]*product[0,k] for k in products)
+                              +gp.quicksum(DAJ[k]*DA_product[0,k] for k in DA_products)),
         name = 'initial_state_of_charge'
     )
 
-    #state of charge in time t is the state of charge in the previous time period + charge added/subtracted by state in time 
+    #state of charge in time t is the state of charge in the previous time period + charge added/subtracted by product in time 
     m.addConstrs(
-        (SoC[t] == SoC[t-1] + gp.quicksum(J[k]*state[t,k] for k in states)
-         + gp.quicksum(DAJ[k]*DA_state[t,k] for k in DA_states) for t in range(1,t_horizon)
+        (SoC[t] == SoC[t-1] + gp.quicksum(J[k]*product[t,k] for k in products)
+         + gp.quicksum(DAJ[k]*DA_product[t,k] for k in DA_products) for t in range(1,t_horizon)
          ),
         name = 'state_of_charge'
     )
@@ -122,19 +121,19 @@ try:
 
         for t in range(t_horizon):
             if model_t_to_day[t] == day:
-                expr_charge.addTerms(J['c'],state[t,'c'])
-                expr_charge.addTerms(J['regd'],state[t,'regd'])
-                expr_charge.addTerms(DAJ['DAc'],DA_state[t,'DAc'])
-                expr_charge.addTerms(DAJ['DAregd'],DA_state[t,'DAregd'])
+                expr_charge.addTerms(J['c'],product[t,'c'])
+                expr_charge.addTerms(J['regd'],product[t,'regd'])
+                expr_charge.addTerms(DAJ['DAc'],DA_product[t,'DAc'])
+                expr_charge.addTerms(DAJ['DAregd'],DA_product[t,'DAregd'])
 
-                expr_discharge.addTerms(J['d'],state[t,'d'])
-                expr_discharge.addTerms(J['spinr'],state[t,'spinr'])
-                expr_discharge.addTerms(J['supr'],state[t,'supr'])
-                expr_discharge.addTerms(J['regu'],state[t,'regu'])
-                expr_discharge.addTerms(DAJ['DAd'],DA_state[t,'DAd'])
-                expr_discharge.addTerms(DAJ['DAspinr'],DA_state[t,'DAspinr'])
-                expr_discharge.addTerms(DAJ['DAsupr'],DA_state[t,'DAsupr'])
-                expr_discharge.addTerms(DAJ['DAregu'],DA_state[t,'DAregu'])
+                expr_discharge.addTerms(J['d'],product[t,'d'])
+                expr_discharge.addTerms(J['spinr'],product[t,'spinr'])
+                expr_discharge.addTerms(J['supr'],product[t,'supr'])
+                expr_discharge.addTerms(J['regu'],product[t,'regu'])
+                expr_discharge.addTerms(DAJ['DAd'],DA_product[t,'DAd'])
+                expr_discharge.addTerms(DAJ['DAspinr'],DA_product[t,'DAspinr'])
+                expr_discharge.addTerms(DAJ['DAsupr'],DA_product[t,'DAsupr'])
+                expr_discharge.addTerms(DAJ['DAregu'],DA_product[t,'DAregu'])
 
 
         m.addConstr(
@@ -147,7 +146,7 @@ try:
             name = 'day %s discharge cycle limit' % day
         )
 
-        #add day ahead states to cycle limit
+        #add day ahead products to cycle limit
         #150% potential in the future, depends on evaluation period. 150% might depend on how much time were solving for
     
     #if we participate in the day ahead, we must participate in the full hour
@@ -160,8 +159,8 @@ try:
             if(len(temp)>1):
                 m.addConstrs(
                     (
-                    gp.quicksum(DA_state[t,k] for t in temp[1:]) == (len(temp)-1)*DA_state[temp[0],k]
-                            for k in DA_states),
+                    gp.quicksum(DA_product[t,k] for t in temp[1:]) == (len(temp)-1)*DA_product[temp[0],k]
+                            for k in DA_products),
                             name = 'commit_hour'
                 )
 
@@ -176,24 +175,24 @@ try:
 
 
     final_SOC = []
-    final_state = []
+    final_product = []
     final_price = []
     for t in df.index:
         final_SOC.append(SoC[t].X)
-        for s in states:
-            if state[t,s].X > 0.9:
-                final_state.append(s)
+        for s in products:
+            if product[t,s].X > 0.9:
+                final_product.append(s)
                 final_price.append(P[s][t])
-        for s in DA_states:
-            if DA_state[t,s].X > 0.9:
-                final_state.append(s)
+        for s in DA_products:
+            if DA_product[t,s].X > 0.9:
+                final_product.append(s)
                 final_price.append(P[s][t])
 
     
     data = {
         'date_time':df['date_time'],
         'SoC':final_SOC,
-        'state':final_state,
+        'product':final_product,
         'price': final_price
     }
     final_SOC_df = pd.DataFrame(data)
