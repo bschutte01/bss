fa97321e8d9cba1aad8553eb5d3e03fe55943c78
bschutte01 <bschutte@unomaha.edu>
2023-11-17 13:14:29 -0600

    modifying code for new data

--- model.py
+++ model.py
@@ -7,18 +7,25 @@ import os
 import datetime
 
 file_loc = os.path.dirname(os.path.realpath(__file__))
-file_name = 'nodal_input.xls'
+file_name = '2022_nodal_input.xlsx'
 path_to_file = Path(file_loc)/file_name
 
 temp = datetime.datetime.now()
 run_id = temp.strftime('%Y%m%d%H%M%S')
 
+print('reading data')
 df = pd.read_excel(path_to_file)
 
+print('creating data columns')
 df['day'] = df['date_time'].dt.date
 df['hour'] = df['date_time'].dt.hour
 
+print('sorting by date')
+df= df.sort_values(by = 'date_time', ignore_index= True)
+df = df.fillna(0)
+
 try:
+    print('Model initialized')
     m = gp.Model('main')
 
     ##########################
@@ -26,12 +33,14 @@ try:
     ##########################
 
     # Time params #
+    print('creating parameters')
     t_delta = 5 #length of a timeframe in minutes
     #days = 1 #num of days to consider
     t_horizon = max(df.index)+1
 
     #model_t_to_day = {[t for t in range(t_horizon)][i]:np.array([day for day in range(days)]).repeat(24 * 60/t_delta)[i] for i in range(t_horizon)}
-    model_t_to_day = {[t for t in list(df.index)][i]:df['day'][i] for i in list(df.index)}
+    #model_t_to_day = {[t for t in list(df.index)][i]:df['day'][i] for i in list(df.index)}
+    model_t_to_day = df['day']
     #gives the corresponding day when given a model period t as the parameter
     #ex: model_t_to_day[7] to get the corresponding day of the 7th model period
 
@@ -45,8 +54,8 @@ try:
     #products the battery can be in
     ### in order they are idle, charge, discharge, spinning reserve,
     ### supplemental reserve, regulation up, regulation down
-    products = ['i','c','d','spinr','supr','regu','regd']
-    DA_products = ['DAi','DAc','DAd','DAspinr','DAsupr','DAregu','DAregd']
+    products = ['i','c','d','spinr','suppr','regu','regd']
+    DA_products = ['DAi','DAc','DAd','DAspinr','DAsuppr','DAregu','DAregd']
 
     # Charging params #
     init_SOC = .25      #state of charge prior to running model
@@ -62,10 +71,10 @@ try:
     #just generate some data randomly, need to get this from an input file
     P = df
     np.random.seed(2023)
-    DAP = pd.DataFrame(np.random.normal(loc = 1.0, scale =15,
-                                        size = (df.shape[0],len(DA_products))),
-                                        columns=DA_products)
-    P = pd.concat([P,DAP], axis = 1)
+    #DAP = pd.DataFrame(np.random.normal(loc = 1.0, scale =15,
+    #                                    size = (df.shape[0],len(DA_products))),
+    #                                    columns=DA_products)
+    #P = pd.concat([P,DAP], axis = 1)
 
 
     # Model Params #
@@ -76,10 +85,10 @@ try:
     ##########################
     ### Decision Variables ###
     ##########################
-
+    print('creating decision variables')
     product = m.addVars(t_horizon,products ,vtype=GRB.BINARY,name = 'product')
     # product[1,'c'] = 0/1
-    # products = ['i','c','d','spinr','supr','regu','regd']
+    # products = ['i','c','d','spinr','suppr','regu','regd']
 
     DA_product = m.addVars(t_horizon,DA_products ,vtype=GRB.BINARY,name = 'DA_product')
 
@@ -90,6 +99,7 @@ try:
     ##########################
     ### Objective Function ###
     ##########################
+    print('setting objective function')
     m.setObjective(gp.quicksum(P[j][i]*product[i,j] for i in range(t_horizon) for j in products)
                    + gp.quicksum(P[j][i]*DA_product[i,j] for i in range(t_horizon) for j in DA_products), 
                    GRB.MAXIMIZE)
@@ -97,14 +107,16 @@ try:
     ##########################
     ###### Constraints #######
     ##########################
-
+    print('building constraints')
     # battery can only be in 1 product in a given timeframe
+    print('...single_assignment')
     m.addConstrs(
         (product.sum(t,'*') + DA_product.sum(t,'*') == 1 for t in range(t_horizon)), 
         name = 'single_assignment'
     )
 
     #intial state of charge
+    print('...initial_state_of_charge')
     m.addConstr(
         SoC[0] == init_SOC + (gp.quicksum(J[k]*product[0,k] for k in products)
                               +gp.quicksum(DAJ[k]*DA_product[0,k] for k in DA_products)),
@@ -112,6 +124,7 @@ try:
     )
 
     #state of charge in time t is the state of charge in the previous time period + charge added/subtracted by product in time 
+    print('...state_of_charge')
     m.addConstrs(
         (SoC[t] == SoC[t-1] + gp.quicksum(J[k]*product[t,k] for k in products)
          + gp.quicksum(DAJ[k]*DA_product[t,k] for k in DA_products) for t in range(1,t_horizon)
@@ -120,6 +133,7 @@ try:
     )
 
     #only one full cycle per 24 hours
+    print('...charge cycle limit')
     for day in df.day.unique():
         expr_charge = gp.LinExpr()
         expr_discharge = gp.LinExpr()
@@ -133,11 +147,11 @@ try:
 
                 expr_discharge.addTerms(J['d'],product[t,'d'])
                 expr_discharge.addTerms(J['spinr'],product[t,'spinr'])
-                expr_discharge.addTerms(J['supr'],product[t,'supr'])
+                expr_discharge.addTerms(J['suppr'],product[t,'suppr'])
                 expr_discharge.addTerms(J['regu'],product[t,'regu'])
                 expr_discharge.addTerms(DAJ['DAd'],DA_product[t,'DAd'])
                 expr_discharge.addTerms(DAJ['DAspinr'],DA_product[t,'DAspinr'])
-                expr_discharge.addTerms(DAJ['DAsupr'],DA_product[t,'DAsupr'])
+                expr_discharge.addTerms(DAJ['DAsuppr'],DA_product[t,'DAsuppr'])
                 expr_discharge.addTerms(DAJ['DAregu'],DA_product[t,'DAregu'])
 
 
@@ -155,6 +169,7 @@ try:
         #150% potential in the future, depends on evaluation period. 150% might depend on how much time were solving for
     
     #if we participate in the day ahead, we must participate in the full hour
+    print('...commit_hour for day ahead')
     for day in df.day.unique():
         for hour in df.hour.unique():
             expr = gp.LinExpr()
@@ -170,15 +185,16 @@ try:
                 )
 
     #need to create for discharging as well
-
+    print('all constraints complete')
     m.write(run_id + '_out.lp')
+    print('beginning optimization')
     m.optimize()
 
     #for v in m.getVars():
     #    if(abs(v.X)>0):
     #        print('%s %g' % (v.VarName, v.X))
 
-
+    print('outputting results')
     final_SOC = []
     final_product = []
     final_price = []
